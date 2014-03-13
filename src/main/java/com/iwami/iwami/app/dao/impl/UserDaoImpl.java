70dd45ef6c72f4445e145945f6908434aabe6ed1
scofieldlin <scofieldlin@hotmail.com>
2014-03-13 22:56:11 +0800

    minor changes

--- src/main/java/com/iwami/iwami/app/dao/impl/UserDaoImpl.java
+++ src/main/java/com/iwami/iwami/app/dao/impl/UserDaoImpl.java
@@ -89,7 +89,7 @@ public class UserDaoImpl extends JdbcDaoSupport implements UserDao {
 
 	@Override
 	public boolean subUserCurrentNExchangePrize(long userid, int prize) {
-		int count = getJdbcTemplate().update("update " + SqlConstants.TABLE_USER + " set current_prize = current_prize - ?, exchange_prize = exchange_prize + ? where id = ? and current_prize >= ?, lastmod_time = now(), lastmod_userid = ? and isdel = 0", new Object[]{prize, prize, userid, userid, prize});
+		int count = getJdbcTemplate().update("update " + SqlConstants.TABLE_USER + " set current_prize = current_prize - ?, exchange_prize = exchange_prize + ?, lastmod_time = now(), lastmod_userid = ? where id = ? and current_prize >= ? and isdel = 0", new Object[]{prize, prize, userid, userid, prize});
 		if(count > 0)
 			return true;
 		else
